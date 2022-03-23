c62ca5174afb40a4ece444abac3ee07135fd6203
Tomasi - Developing <info@tomasi-developing.ch>
2022-03-23 08:54:43 +0100

    Implement graphQL

--- GraphQlTest/Program.cs
+++ GraphQlTest/Program.cs
@@ -1,5 +1,12 @@
+using GraphiQl;
+using GraphQL.Server;
+using GraphQL.Types;
 using GraphQlTest.Interfaces;
+using GraphQlTest.Mutation;
+using GraphQlTest.Query;
+using GraphQlTest.Schema;
 using GraphQlTest.Services;
+using GraphQlTest.Type;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,14 +16,21 @@ builder.Services.AddControllers();
 
 builder.Services.AddTransient<IProduct, ProductService>();
 
+builder.Services.AddSingleton<ProductType>();
+builder.Services.AddSingleton<ProductQuery>();
+builder.Services.AddSingleton<ProductMutation>();
+builder.Services.AddSingleton<ISchema, ProductSchema>();
+
+builder.Services.AddGraphQL(options =>
+{
+    options.EnableMetrics = false;
+}).AddSystemTextJson();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
+app.UseGraphiQl("/graphql");
+app.UseGraphQL<ISchema>();
 
 app.Run();
---
+++ GraphQlTest/Schema/ProductSchema.cs
@@ -0,0 +1,14 @@
+﻿using GraphQlTest.Mutation;
+using GraphQlTest.Query;
+
+namespace GraphQlTest.Schema
+{
+    public class ProductSchema : GraphQL.Types.Schema
+    {
+        public ProductSchema(ProductQuery productQuery, ProductMutation productMutation)
+        {
+            Query = productQuery;
+            Mutation = productMutation;
+        }
+    }
+}
